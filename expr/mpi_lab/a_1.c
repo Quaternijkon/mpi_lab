cad7b7d876e42f29ddacc5f71892e9666cd05ab1
Quaternijkon <Quaternijkon@mail.ustc.edu.cn>
2024-12-13 21:42:54 +0800

    fix

--- expr/mpi_lab/a_1.c
+++ expr/mpi_lab/a_1.c
@@ -5,38 +5,27 @@
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
 
+    MPI_Comm world_comm = MPI_COMM_WORLD;
     int world_rank, world_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(world_comm, &world_rank);
+    MPI_Comm_size(world_comm, &world_size);
 
     MPI_Comm node_comm;
-    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &node_comm);
+    MPI_Comm_split_type(world_comm, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &node_comm);
 
     int node_rank, node_size;
     MPI_Comm_rank(node_comm, &node_rank);
     MPI_Comm_size(node_comm, &node_size);
 
-    int node_id;
-    if (node_rank == 0) {
-        node_id = world_rank; 
-    }
-    MPI_Bcast(&node_id, 1, MPI_INT, 0, node_comm);
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
 
-    int total_nodes;
-    MPI_Comm node_leader_comm;
-    MPI_Comm_split(MPI_COMM_WORLD, node_rank == 0, node_id, &node_leader_comm);
-
-    if (node_rank == 0) {
-        MPI_Comm_size(node_leader_comm, &total_nodes);
-    }
-    MPI_Bcast(&total_nodes, 1, MPI_INT, 0, node_leader_comm);
-
-    if (node_rank == 0) {
-        printf("Node %d has %d processes\n", node_id, node_size);
-    }
+    printf("全局排名: %d, 节点内排名: %d/%d, 节点名称: %s\n", 
+           world_rank, node_rank, node_size, processor_name);
 
     MPI_Comm_free(&node_comm);
-    MPI_Comm_free(&node_leader_comm);
+
     MPI_Finalize();
     return 0;
 }
