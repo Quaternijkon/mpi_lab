81916a525eec209be4bd31386bcecf22c76de670
Quaternijkon <Quaternijkon@mail.ustc.edu.cn>
2024-12-13 22:58:03 +0800

    fix

--- expr/mpi_lab/lab-b.c
+++ expr/mpi_lab/lab-b.c
@@ -4,123 +4,118 @@
 #include <string.h>
 #include <time.h>
 
-#define ROOT 0
-#define TAG 100
-#define MAX_OUTPUT_SIZE 256  
 
-int main(int argc, char *argv[]) {
-    MPI_Init(&argc, &argv);
+int My_Alltoall(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
+               void *recvbuf, int recvcount, MPI_Datatype recvtype,
+               MPI_Comm comm) {
+    int rank, size, i;
+    MPI_Status status;
 
-    int world_rank, world_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
 
-    int send_count = 1000; 
-    int recv_count = 1000;
+    int send_type_size, recv_type_size;
+    MPI_Type_size(sendtype, &send_type_size);
+    MPI_Type_size(recvtype, &recv_type_size);
 
-    int *send_buffer = (int *)malloc(send_count * world_size * sizeof(int));
-    int *recv_buffer = (int *)malloc(recv_count * world_size * sizeof(int));
-
-    for(int i = 0; i < send_count * world_size; i++) {
-        send_buffer[i] = world_rank;
-    }
-
-    double start_time = MPI_Wtime();
-
-    for(int i = 0; i < world_size; i++) {
-        if(i == world_rank) {
-            memcpy(&recv_buffer[i * recv_count], &send_buffer[i * send_count], send_count * sizeof(int));
+    
+    for (i = 0; i < size; i++) {
+        if (i == rank) {
+            
+            memcpy((char *)recvbuf + i * recvcount * recv_type_size,
+                   (char *)sendbuf + i * sendcount * send_type_size,
+                   sendcount * send_type_size);
         } else {
-            MPI_Send(&send_buffer[i * send_count], send_count, MPI_INT, i, TAG, MPI_COMM_WORLD);
-            MPI_Recv(&recv_buffer[i * recv_count], recv_count, MPI_INT, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            MPI_Send((char *)sendbuf + i * sendcount * send_type_size,
+                     sendcount, sendtype, i, 0, comm);
+            
+            MPI_Recv((char *)recvbuf + i * recvcount * recv_type_size,
+                     recvcount, recvtype, i, 0, comm, &status);
         }
     }
 
-    double end_time = MPI_Wtime();
-    double simulation_time = end_time - start_time;
+    return MPI_SUCCESS;
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    const int num_iterations = 100; 
+    double start_time, end_time;
+    double my_alltoall_time = 0.0, mpi_alltoall_time = 0.0;
+    int i;
+    int message_size = 1024; 
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     
-    char output_simulation[MAX_OUTPUT_SIZE];
-    snprintf(output_simulation, MAX_OUTPUT_SIZE, "Process %d: Fake MPI_Alltoall Time = %f seconds\n", world_rank, simulation_time);
+    char *sendbuf = (char *)malloc(size * message_size * sizeof(char));
+    char *recvbuf_my = (char *)malloc(size * message_size * sizeof(char));
+    char *recvbuf_mpi = (char *)malloc(size * message_size * sizeof(char));
 
     
-    printf("%s", output_simulation);
-
-    free(send_buffer);
-    free(recv_buffer);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    
-    send_buffer = (int *)malloc(send_count * world_size * sizeof(int));
-    recv_buffer = (int *)malloc(recv_count * world_size * sizeof(int));
-
-    for(int i = 0; i < send_count * world_size; i++) {
-        send_buffer[i] = world_rank;
-    }
-
-    start_time = MPI_Wtime();
-
-    MPI_Alltoall(send_buffer, send_count, MPI_INT, recv_buffer, recv_count, MPI_INT, MPI_COMM_WORLD);
-
-    end_time = MPI_Wtime();
-    double alltoall_time = end_time - start_time;
-
-    
-    char output_alltoall[MAX_OUTPUT_SIZE];
-    snprintf(output_alltoall, MAX_OUTPUT_SIZE, "Process %d: MPI_Alltoall Time = %f seconds\n", world_rank, alltoall_time);
-
-    
-    printf("%s", output_alltoall);
-
-    free(send_buffer);
-    free(recv_buffer);
-
-    
-    char *all_outputs = NULL;
-    if(world_rank == ROOT) {
-        all_outputs = (char *)malloc(world_size * MAX_OUTPUT_SIZE * sizeof(char));
+    for (i = 0; i < size * message_size; i++) {
+        sendbuf[i] = 'a' + (rank % 26);
     }
 
     
-    MPI_Gather(output_simulation, MAX_OUTPUT_SIZE, MPI_CHAR,
-               all_outputs, MAX_OUTPUT_SIZE, MPI_CHAR,
-               ROOT, MPI_COMM_WORLD);
-
-    
-    char *all_outputs_alltoall = NULL;
-    if(world_rank == ROOT) {
-        all_outputs_alltoall = (char *)malloc(world_size * MAX_OUTPUT_SIZE * sizeof(char));
+    for (i = 0; i < num_iterations; i++) {
+        memset(recvbuf_my, 0, size * message_size);
+        MPI_Barrier(MPI_COMM_WORLD);
+        start_time = MPI_Wtime();
+        My_Alltoall(sendbuf, message_size, MPI_CHAR,
+                    recvbuf_my, message_size, MPI_CHAR, MPI_COMM_WORLD);
+        end_time = MPI_Wtime();
+        my_alltoall_time += (end_time - start_time);
     }
 
-    MPI_Gather(output_alltoall, MAX_OUTPUT_SIZE, MPI_CHAR,
-               all_outputs_alltoall, MAX_OUTPUT_SIZE, MPI_CHAR,
-               ROOT, MPI_COMM_WORLD);
+    
+    for (i = 0; i < num_iterations; i++) {
+        memset(recvbuf_mpi, 0, size * message_size);
+        MPI_Barrier(MPI_COMM_WORLD);
+        start_time = MPI_Wtime();
+        MPI_Alltoall(sendbuf, message_size, MPI_CHAR,
+                     recvbuf_mpi, message_size, MPI_CHAR, MPI_COMM_WORLD);
+        end_time = MPI_Wtime();
+        mpi_alltoall_time += (end_time - start_time);
+    }
 
     
-    if(world_rank == ROOT) {
-        FILE *fp = fopen("combined_output.txt", "w");
-        if(fp == NULL){
-            fprintf(stderr, "Root process: 无法打开文件 combined_output.txt\n");
+    double avg_my_alltoall = my_alltoall_time / num_iterations;
+    double avg_mpi_alltoall = mpi_alltoall_time / num_iterations;
+
+    
+    double max_my_alltoall, max_mpi_alltoall;
+    MPI_Reduce(&avg_my_alltoall, &max_my_alltoall, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&avg_mpi_alltoall, &max_mpi_alltoall, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    
+    if (rank == 0) {
+        FILE *fp = fopen("mpi_alltoall_results.txt", "w");
+        if (fp == NULL) {
+            fprintf(stderr, "Error opening file for writing.\n");
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
 
-        
-        fprintf(fp, "Fake MPI_Alltoall Results:\n");
-        for(int i = 0; i < world_size; i++) {
-            fprintf(fp, "%s", &all_outputs[i * MAX_OUTPUT_SIZE]);
-        }
-
-        
-        fprintf(fp, "\nMPI_Alltoall Results:\n");
-        for(int i = 0; i < world_size; i++) {
-            fprintf(fp, "%s", &all_outputs_alltoall[i * MAX_OUTPUT_SIZE]);
-        }
+        fprintf(fp, "MPI_Alltoall Performance Comparison\n");
+        fprintf(fp, "Number of Processes: %d\n", size);
+        fprintf(fp, "Message Size: %d bytes per send\n", message_size);
+        fprintf(fp, "Iterations: %d\n\n", num_iterations);
+        fprintf(fp, "Custom My_Alltoall Average Time: %f seconds\n", max_my_alltoall);
+        fprintf(fp, "MPI_Alltoall Average Time: %f seconds\n", max_mpi_alltoall);
+        fprintf(fp, "Speedup (MPI_Alltoall / My_Alltoall): %f\n",
+                max_my_alltoall / max_mpi_alltoall);
 
         fclose(fp);
-        free(all_outputs);
-        free(all_outputs_alltoall);
+        printf("Results saved to mpi_alltoall_results.txt\n");
     }
+
+    
+    free(sendbuf);
+    free(recvbuf_my);
+    free(recvbuf_mpi);
 
     MPI_Finalize();
     return 0;
