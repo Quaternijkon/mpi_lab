f01ef4a262436a440781121faae502aad8e2d8f8
Quaternijkon <Quaternijkon@mail.ustc.edu.cn>
2024-12-13 23:15:20 +0800

    fix

--- expr/mpi_lab/lab-c-1.c
+++ expr/mpi_lab/lab-c-1.c
@@ -1,77 +1,56 @@
 #include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
 
-/**
- * 检查进程数是否为2的幂次方
- */
+
 int is_power_of_two(int n) {
-    return (n != 0) && ((n & (n - 1)) == 0);
+    return (n != 0) && ((n & (n -1)) == 0);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char** argv) {
     int rank, size;
-    double local_value; 
-    double total_sum = 0.0; 
-    int log_p; 
-    int step;
-    int partner;
+    int logN;
+    double local_data, local_sum, recv_sum;
+    MPI_Status status;
 
+    
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     
     if (!is_power_of_two(size)) {
         if (rank == 0) {
-            fprintf(stderr, "Error: Number of processes must be a power of 2.\n");
+            fprintf(stderr, "进程数必须是2的幂次方。\n");
         }
         MPI_Finalize();
         exit(EXIT_FAILURE);
     }
 
     
-    log_p = (int)(log((double)size) / log(2.0));
-    if (pow(2, log_p) != size) {
-        if (rank == 0) {
-            fprintf(stderr, "Error: Number of processes must be a power of 2.\n");
-        }
-        MPI_Finalize();
-        exit(EXIT_FAILURE);
+    logN = (int)(log((double)size) / log(2.0));
+
+    
+    local_data = (double)(rank + 1);
+    local_sum = local_data;
+
+    
+    for (int i = 0; i < logN; i++) {
+        int partner = rank ^ (1 << i); 
+
+        
+        MPI_Send(&local_sum, 1, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD);
+        MPI_Recv(&recv_sum, 1, MPI_DOUBLE, partner, 0, MPI_COMM_WORLD, &status);
+
+        
+        local_sum += recv_sum;
     }
 
     
-    local_value = (double)rank;
+    printf("进程 %d 的全和结果: %f\n", rank, local_sum);
 
     
-    printf("Process %d initial value: %f\n", rank, local_value);
-
-    
-    for (step = 0; step < log_p; step++) {
-        
-        partner = rank ^ (1 << step);
-
-        double recv_value;
-
-        
-        MPI_Sendrecv(&local_value, 1, MPI_DOUBLE, partner, 0,
-                     &recv_value, 1, MPI_DOUBLE, partner, 0,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        
-        local_value += recv_value;
-
-        
-        printf("Process %d after step %d has value: %f\n", rank, step, local_value);
-    }
-
-    
-    total_sum = local_value;
-
-    
-    printf("Process %d total sum: %f\n", rank, total_sum);
-
     MPI_Finalize();
     return 0;
 }
