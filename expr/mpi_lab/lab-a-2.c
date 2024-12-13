9ae29f8e19d7e91de8665faad5e4721da6fce974
Quaternijkon <Quaternijkon@mail.ustc.edu.cn>
2024-12-13 21:56:16 +0800

    fix

--- expr/mpi_lab/lab-a-2.c
+++ expr/mpi_lab/lab-a-2.c
@@ -24,36 +24,31 @@ int main(int argc, char *argv[]) {
     printf("全局排名: %d, 节点内排名: %d/%d, 节点名称: %s\n", 
            world_rank, node_rank, node_size, processor_name);
 
-    int global_root = 0;
+    MPI_Comm node_roots_comm;
+    int color = (node_rank == 0) ? 0 : MPI_UNDEFINED;
+    MPI_Comm_split(world_comm, color, world_rank, &node_roots_comm);
 
-    MPI_Comm group_roots_comm;
-    int is_group_root = (node_rank == 0) ? 1 : MPI_UNDEFINED;
-    MPI_Comm_split(world_comm, is_group_root, world_rank, &group_roots_comm);
-
-    char message[100];
-    memset(message, 0, sizeof(message));
-
-    if (is_group_root) {
-        int group_roots_rank, group_roots_size;
-        MPI_Comm_rank(group_roots_comm, &group_roots_rank);
-        MPI_Comm_size(group_roots_comm, &group_roots_size);
-
-        if (world_rank == global_root) {
-            strcpy(message, "Hello from global root");
+    char message[256];
+    if (node_rank == 0) {
+        if (world_rank == 0) {
+            strcpy(message, "Hello from the global root process!");
         }
-
-        MPI_Bcast(message, sizeof(message), MPI_CHAR, 0, group_roots_comm);
-
-        MPI_Bcast(message, sizeof(message), MPI_CHAR, 0, node_comm);
-    } else {
-        MPI_Bcast(message, sizeof(message), MPI_CHAR, 0, node_comm);
     }
 
-    printf("全局排名: %d, 节点内排名: %d/%d, 节点名称: %s, 接收到消息: %s\n", 
-           world_rank, node_rank, node_size, processor_name, message);
+    if (color == 0) {
+        MPI_Bcast(message, sizeof(message), MPI_CHAR, 
+                  (world_rank == 0) ? 0 : MPI_UNDEFINED, node_roots_comm);
+    }
 
-    if (is_group_root) {
-        MPI_Comm_free(&group_roots_comm);
+    MPI_Barrier(world_comm);
+
+    MPI_Bcast(message, sizeof(message), MPI_CHAR, 0, node_comm);
+
+    printf("全局排名: %d, 节点内排名: %d/%d, 接收到的消息: %s\n", 
+           world_rank, node_rank, node_size, message);
+
+    if (color == 0) {
+        MPI_Comm_free(&node_roots_comm);
     }
     MPI_Comm_free(&node_comm);
 
