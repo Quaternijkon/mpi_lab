a4741b6385ccfbcc1e701948f68da04ae1499c8b
Quaternijkon <Quaternijkon@mail.ustc.edu.cn>
2024-12-13 21:36:05 +0800

    update a_1.c

--- expr/mpi_lab/a_1.c
+++ expr/mpi_lab/a_1.c
@@ -17,16 +17,26 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(node_comm, &node_size);
 
     int node_id;
-    MPI_Comm_split(MPI_COMM_WORLD, 0, world_rank, &node_comm);
-    MPI_Comm_rank(node_comm, &node_id);
+    if (node_rank == 0) {
+        node_id = world_rank; 
+    }
+    MPI_Bcast(&node_id, 1, MPI_INT, 0, node_comm);
 
-    if (world_rank == 0) {
-        printf("Total processes: %d\n", world_size);
+    int total_nodes;
+    MPI_Comm node_leader_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, node_rank == 0, node_id, &node_leader_comm);
+
+    if (node_rank == 0) {
+        MPI_Comm_size(node_leader_comm, &total_nodes);
+    }
+    MPI_Bcast(&total_nodes, 1, MPI_INT, 0, node_leader_comm);
+
+    if (node_rank == 0) {
+        printf("Node %d has %d processes\n", node_id, node_size);
     }
 
-    printf("World Rank: %d, Node Rank: %d, Node Size: %d\n", world_rank, node_rank, node_size);
-
     MPI_Comm_free(&node_comm);
+    MPI_Comm_free(&node_leader_comm);
     MPI_Finalize();
     return 0;
 }
