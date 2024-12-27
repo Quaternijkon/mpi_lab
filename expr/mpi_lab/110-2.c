9893745186168ee0d761aa90a50d4e3271cc80d1
Quaternijkon <Quaternijkon@mail.ustc.edu.cn>
2024-12-27 23:39:53 +0800

    fix

--- expr/mpi_lab/110-2.c
+++ expr/mpi_lab/110-2.c
@@ -5,33 +5,29 @@
 #include <math.h>
 
 #ifndef N
-#define N 7
+#define N 50
 #endif
 
 #define INDEX(i, j) (((i)*N)+(j))
 
 void random_array(double *a, int num) {
-    srand(12345);  
     for(int i = 0; i < num; i++) {
-        a[i] = rand() % 100;  
+        srand(time(NULL));
+        a[i] = rand() % 100;
     }
 }
 
 void comp(double *A, double *B, int a, int b) {
-    for (int i = 1; i <= a; i++) {
-        for (int j = 1; j <= b; j++) {
-            int up = (i - 1 + a) % a;
-            int down = (i + 1) % a;
-            int left = (j - 1 + b) % b;
-            int right = (j + 1) % b;
-            B[INDEX(i, j)] = (A[INDEX(up, j)] + A[INDEX(i, right)] + A[INDEX(down, j)] + A[INDEX(i, left)]) / 4.0;
+    for(int i = 1; i <= a; i++) {
+        for(int j = 1; j <= b; j++) {
+            B[INDEX(i, j)] = (A[INDEX(i-1, j)] + A[INDEX(i, j+1)] + A[INDEX(i+1, j)] + A[INDEX(i, j-1)]) / 4.0;
         }
     }
 }
 
 int check(double *B, double *C) {
-    for (int i = 1; i < N - 1; i++) {
-        for (int j = 1; j < N - 1; j++) {
+    for(int i = 1; i < N-1; i++) {
+        for(int j = 1; j < N-1; j++) {
             if (fabs(B[INDEX(i, j)] - C[INDEX(i, j)]) >= 1e-2) {
                 printf("B[%d,%d] = %lf not %lf!\n", i, j, B[INDEX(i, j)], C[INDEX(i, j)]);
                 return 0;
@@ -41,10 +37,10 @@ int check(double *B, double *C) {
     return 1;
 }
 
-void print_matrix(double *mat) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%6.2f ", mat[INDEX(i, j)]);
+void print_matrix(double *matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%lf ", matrix[INDEX(i, j)]);
         }
         printf("\n");
     }
@@ -52,45 +48,45 @@ void print_matrix(double *mat) {
 
 int main(int argc, char *argv[]) {
     double *A, *B, *B2;
+    
     int id_procs, num_procs;
     MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &id_procs);
 
-    int rows = sqrt(num_procs);    
-    int cols = num_procs / rows;   
-    int a = (N - 2 + rows - 1) / rows;  
-    int b = (N - 2 + cols - 1) / cols;  
-
-    
-    A = (double*)malloc((a + 2) * (b + 2) * sizeof(double));  
-    B = (double*)malloc((a + 2) * (b + 2) * sizeof(double));
-    B2 = (double*)malloc(N * N * sizeof(double));  
+    MPI_Datatype SubMat;
+    int rows = sqrt(num_procs);
+    int cols = num_procs / rows;
+    int a = (N-2 + rows-1) / rows;
+    int b = (N-2 + cols-1) / cols;
+    int alloc_num = (a+1)*(b+1)*num_procs;
+    A = (double*)malloc(alloc_num * sizeof(double));
+    B = (double*)malloc(alloc_num * sizeof(double));
+    B2 = (double*)malloc(alloc_num * sizeof(double));
 
     
     if (id_procs == 0) {
         random_array(A, N * N);
-        comp(A, B2, N - 2, N - 2);
+        comp(A, B2, N-2, N-2);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
 
     
-    MPI_Datatype SubMat;
-    MPI_Type_vector(a + 2, b + 2, N, MPI_DOUBLE, &SubMat);
+    MPI_Type_vector(a+2, b+2, N, MPI_DOUBLE, &SubMat);
     MPI_Type_commit(&SubMat);
 
-    
     if (id_procs == 0) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
                 if (i == 0 && j == 0)
                     continue;
                 MPI_Send(A + i * a * N + b * j, 1, SubMat, j + cols * i, 0, MPI_COMM_WORLD);
             }
         }
-    } else {
+    }
+    else {
         MPI_Recv(A, 1, SubMat, 0, 0, MPI_COMM_WORLD, &status);
     }
 
@@ -101,7 +97,6 @@ int main(int argc, char *argv[]) {
     MPI_Datatype SubMat_B;
     MPI_Type_vector(a, b, N, MPI_DOUBLE, &SubMat_B);
     MPI_Type_commit(&SubMat_B);
-
     if (id_procs == 0) {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -116,7 +111,6 @@ int main(int argc, char *argv[]) {
         MPI_Send(&B[INDEX(1, 1)], 1, SubMat_B, 0, 1, MPI_COMM_WORLD);
     }
 
-    
     if (id_procs == 0) {
         if (check(B, B2)) {
             printf("Done. No Error\n");
@@ -125,18 +119,16 @@ int main(int argc, char *argv[]) {
         }
 
         
-        printf("\nMatrix A (initialized with indices):\n");
-        print_matrix(A);
+        printf("\nMatrix A:\n");
+        print_matrix(A, N, N);
 
-        printf("\nMatrix B (calculated result):\n");
-        print_matrix(B);
+        printf("\nMatrix B (after computation):\n");
+        print_matrix(B, N, N);
     }
 
-    
     free(A);
     free(B);
     free(B2);
-
     MPI_Finalize();
     return 0;
 }
