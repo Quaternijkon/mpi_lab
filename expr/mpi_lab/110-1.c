85b0ca3a341a65c276725fd42ceeea57435493a1
Quaternijkon <Quaternijkon@mail.ustc.edu.cn>
2024-12-28 11:39:05 +0800

    fix

--- expr/mpi_lab/110-1.c
+++ expr/mpi_lab/110-1.c
@@ -4,12 +4,11 @@
 #include <time.h>
 #include <math.h>
 
-#ifndef N
-#define N 7
-#endif
+// 移除宏定义，并使用全局变量 N
+int N = 7;
 
-#define INDEX(i, j) (((i)*N)+(j))
-
+// 修改 INDEX 宏以使用全局变量 N
+#define INDEX(i, j) (((i) * N) + (j))
 
 void random_array(double *a, int num) {
     for (int i = 0; i < N; i++) {
@@ -18,7 +17,6 @@ void random_array(double *a, int num) {
         }
     }
 
-    
     printf("Matrix A (initialized with indices):\n");
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -28,7 +26,6 @@ void random_array(double *a, int num) {
     }
 }
 
-
 void initialize_matrix(double *B) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -36,7 +33,6 @@ void initialize_matrix(double *B) {
         }
     }
 }
-
 
 void comp(double *A, double *B) {
     for (int i = 0; i < N; i++) {
@@ -51,7 +47,6 @@ void comp(double *A, double *B) {
     }
 }
 
-
 int check(double *B, double *C) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -64,7 +59,6 @@ int check(double *B, double *C) {
     return 1;
 }
 
-
 void print_matrix(double *mat) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -76,9 +70,9 @@ void print_matrix(double *mat) {
 
 int main(int argc, char *argv[]) {
     double *A, *B, *B2;
-    A = (double*)malloc(N*N*sizeof(double));
-    B = (double*)malloc(N*N*sizeof(double));
-    B2 = (double*)malloc(N*N*sizeof(double));
+    A = (double*)malloc(N * N * sizeof(double));
+    B = (double*)malloc(N * N * sizeof(double));
+    B2 = (double*)malloc(N * N * sizeof(double));
 
     int id_procs, num_procs, num_1;
     MPI_Status status;
@@ -88,73 +82,110 @@ int main(int argc, char *argv[]) {
 
     num_1 = num_procs - 1;
 
-    
+    // 主进程解析命令行参数并广播 N
     if (id_procs == num_1) {
-        random_array(A, N*N);
+        if (argc >= 2) {
+            N = atoi(argv[1]);
+            if (N <= 0) {
+                if (id_procs == num_1) {
+                    printf("Invalid matrix size provided. Using default N = 7.\n");
+                }
+                N = 7;
+            }
+        }
+    }
+
+    // 广播 N 给所有进程
+    MPI_Bcast(&N, 1, MPI_INT, num_1, MPI_COMM_WORLD);
+
+    if (id_procs == num_1) {
+        // 根据新的 N 分配内存
+        free(A);
+        free(B);
+        free(B2);
+        A = (double*)malloc(N * N * sizeof(double));
+        B = (double*)malloc(N * N * sizeof(double));
+        B2 = (double*)malloc(N * N * sizeof(double));
+
+        random_array(A, N * N);
         initialize_matrix(B);
         comp(A, B2);
     }
 
+    // 同步所有进程
     MPI_Barrier(MPI_COMM_WORLD);
 
+    // 开始计时
+    double start_time, end_time, elapsed_time, max_time;
+    start_time = MPI_Wtime();
+
     int ctn = 0;
-    for (int i = 0; i < N-2; i++) {
+    for (int i = 0; i < N - 2; i++) {
         if (id_procs == num_1) {
             int dest = i % num_1;
             int tag = i / num_1;
-            MPI_Send(&A[INDEX(i, 0)], N*3, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
+            MPI_Send(&A[INDEX(i, 0)], N * 3, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
         }
     }
 
-    for (int i = 0; i < (N-2)/num_1; i++) {
+    for (int i = 0; i < (N - 2) / num_1; i++) {
         if (id_procs != num_1) {
-            MPI_Recv(&A[INDEX(3*ctn, 0)], 3*N, MPI_DOUBLE, num_1, ctn, MPI_COMM_WORLD, &status);
+            MPI_Recv(&A[INDEX(3 * ctn, 0)], 3 * N, MPI_DOUBLE, num_1, ctn, MPI_COMM_WORLD, &status);
             ctn++;
         }
     }
-    if (id_procs < (N-2) % num_1) {
-        MPI_Recv(&A[INDEX(ctn*3, 0)], 3*N, MPI_DOUBLE, num_1, ctn, MPI_COMM_WORLD, &status);
-        ctn++;
+    if (id_procs < (N - 2) % num_1) {
+        if (id_procs != num_1) { // 确保只有非主进程接收
+            MPI_Recv(&A[INDEX(ctn * 3, 0)], 3 * N, MPI_DOUBLE, num_1, ctn, MPI_COMM_WORLD, &status);
+            ctn++;
+        }
     }
 
-    
     if (id_procs != num_1) {
         for (int i = 1; i <= ctn; i++) {
-            for (int j = 1; j < N-1; j++) {
-                B[INDEX(i, j)] = (A[INDEX(i-1, j)] + A[INDEX(i, j+1)] + A[INDEX(i+1, j)] + A[INDEX(i, j-1)]) / 4.0;
+            for (int j = 1; j < N - 1; j++) {
+                B[INDEX(i, j)] = (A[INDEX(i - 1, j)] + A[INDEX(i, j + 1)] + A[INDEX(i + 1, j)] + A[INDEX(i, j - 1)]) / 4.0;
             }
         }
     }
 
-    
-    for (int i = 0; i < N-2; i++) {
+    for (int i = 0; i < N - 2; i++) {
         if (id_procs == num_1) {
             int src = i % num_1;
-            MPI_Recv(&B[INDEX(i+1, 1)], N-2, MPI_DOUBLE, src, i/num_1+N, MPI_COMM_WORLD, &status);
+            MPI_Recv(&B[INDEX(i + 1, 1)], N - 2, MPI_DOUBLE, src, i / num_1 + N, MPI_COMM_WORLD, &status);
         } else {
             for (int j = 0; j < ctn; j++) {
-                MPI_Send(&B[INDEX(j+1, 1)], N-2, MPI_DOUBLE, num_1, j+N, MPI_COMM_WORLD);
+                MPI_Send(&B[INDEX(j + 1, 1)], N - 2, MPI_DOUBLE, num_1, j + N, MPI_COMM_WORLD);
             }
         }
     }
 
-    
+    // 停止计时
+    end_time = MPI_Wtime();
+    elapsed_time = end_time - start_time;
+
+    // 计算所有进程的最大用时
+    MPI_Reduce(&elapsed_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, num_1, MPI_COMM_WORLD);
+
     if (id_procs == num_1) {
+        // 如果需要检查结果，可以取消注释以下代码
+        /*
         if (check(B, B2)) {
             printf("Done. No Error\n");
         } else {
             printf("Error Occurred!\n");
         }
+        */
 
-        
         printf("\nMatrix A (initialized with indices):\n");
         print_matrix(A);
 
         printf("\nMatrix B (calculated result):\n");
         print_matrix(B);
+
+        printf("\nTime elapsed for computing matrix B: %lf seconds\n", max_time);
     }
 
-    
     free(A);
     free(B);
     free(B2);
